aaf180459586d61206ce0f8dd5e2429a44eeb74b
Matt <matt.voget@gmail.com>
2016-07-03 11:32:20 -0600

    Added support functions for making mdl textfields dirty, clean, and valid

--- src/js/ng-mdl-utils.service.js
+++ src/js/ng-mdl-utils.service.js
@@ -13,11 +13,35 @@ function MdlUtils() {
 
   // Function declarations
   mdlConfirmService.closeDrawer = closeDrawer;
+  mdlConfirmService.makeMdlInputsDirty = makeMdlInputsDirty;
+  mdlConfirmService.makeMdlInputsClean = makeMdlInputsClean;
+  mdlConfirmService.makeMdlInputsValid = makeMdlInputsValid;
 
   // Function implementations
   function closeDrawer(){
     document.body.querySelector('.mdl-layout__obfuscator.is-visible').click();
   };
+
+  function makeMdlInputsDirty(){
+    var inputs = document.querySelectorAll('.mdl-textfield');
+    for (var i=0; i<inputs.length; i++){
+      inputs[i].classList.add('is-dirty');
+    }
+  }
+
+  function makeMdlInputsClean(){
+    var inputs = document.querySelectorAll('.mdl-textfield');
+    for (var i=0; i<inputs.length; i++){
+      inputs[i].classList.remove('is-dirty');
+    }
+  }
+
+  function makeMdlInputsValid(){
+    var inputs = document.querySelectorAll('.mdl-textfield');
+    for (var i=0; i<inputs.length; i++){
+      inputs[i].classList.remove('is-invalid');
+    }
+  }
 
   // Return the service
   return mdlConfirmService;
